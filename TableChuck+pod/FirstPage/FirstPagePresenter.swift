202b9ca318e11c65c297163fc3ce4e9b4aa4d4a0
Vladislav Pashkevich <vlad.pashkev@gmail.com>
2021-11-03 18:54:54 +0300

    03-11-2021 the end

--- TableChuck+pod/FirstPage/FirstPagePresenter.swift
+++ TableChuck+pod/FirstPage/FirstPagePresenter.swift
@@ -38,47 +38,50 @@ class FirstPagePresenter: FirstPagePresenterProtocol {
             context: DatabaseService.shared.persistentContainer.mainContext,
             closure: { value in
                 self.categoriesNow = value
+                
+                if self.categoriesNow.count == 0 {
+                    self.view?.activeMBProgressHUD()
+                    guard let url = URL(string: "https://api.chucknorris.io/jokes/categories") else {
+                        return
+                    }
+                    // [String].self -  если возвращается с сервера конкретные данные - если возвращается в формате json ключ: значение - то надо делать структуру и возвращаться только то что нам надо и проиписывать вместо string  - name struct
+                    
+                    AF.request(url, method: .get).responseDecodable(of: [String].self) { response in
+                        switch response.result {
+                        case .success(let result):
+                            var categoriesValue = result
+                            //сохран в CoreData
+                            categoriesValue.insert("Random", at: 0)
+                            // пробигаемся по полученному массиву
+                            for value in categoriesValue {
+                                DatabaseService.shared.insertEntityFor(
+                                    type: Categories.self,
+                                    context: DatabaseService.shared.persistentContainer.mainContext,
+                                    closure: { nameCategory in
+                                        nameCategory.note = value
+                                        self.categoriesNow.append(nameCategory)
+                                        DatabaseService.shared.saveMain(nil)
+                                        print(self.categoriesNow)
+                                        self.view?.reloadData()
+                                        self.view?.deactiveMBProgressHUD()
+                                    }
+                                )
+                                //в CoreData хранится не массив а элементы --- а получаем из CoreData массив
+                            }
+                            
+                        case .failure(let err):
+                            self.view?.showAlert(message: err.localizedDescription)
+                        }
+                    }
+                } else {
+                    print("CoreData categories is not Empty")
+                    self.view?.reloadData()
+                    self.view?.deactiveMBProgressHUD()
+                }
             }
         )
         
-        print(categoriesNow)
-        if categoriesNow.count == 0 {
-            self.view?.activeMBProgressHUD()
-            guard let url = URL(string: "https://api.chucknorris.io/jokes/categories") else {
-                return
-            }
-            // [String].self -  если возвращается с сервера конкретные данные - если возвращается в формате json ключ: значение - то надо делать структуру и возвращаться только то что нам надо и проиписывать вместо string  - name struct
-            
-            AF.request(url, method: .get).responseDecodable(of: [String].self) { response in
-                switch response.result {
-                case .success(let result):
-                    var categoriesValue = result
-                    //сохран в CoreData
-                    categoriesValue.insert("Random", at: 0)
-                    // пробигаемся по полученному массиву
-                    for value in categoriesValue {
-                        DatabaseService.shared.insertEntityFor(
-                            type: Categories.self,
-                            context: DatabaseService.shared.persistentContainer.mainContext,
-                            closure: { nameCategory in
-                                nameCategory.note = value
-                                self.categoriesNow.append(nameCategory)
-                                DatabaseService.shared.saveMain(nil)
-                                print(self.categoriesNow)
-                                self.view?.reloadData()
-                                self.view?.deactiveMBProgressHUD()
-                            }
-                        )
-                        //в CoreData хранится не массив а элементы --- а получаем из CoreData массив
-                    }
-                    
-                case .failure(let err):
-                    self.view?.showAlert(message: err.localizedDescription)
-                }
-            }
-        } else {
-            print("CoreData categories is not Empty")
-        }
+        
     }
     
     func categoryName(for indexPath: IndexPath) -> String {
